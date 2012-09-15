48db2b199b9e15ddeb60c3d078ec82e409f14c71
Michel Meyer <micmeyer@uos.de>
2012-09-15 15:03:34 +0200

    added decimal thousands grouping method

--- src/util/OilImpDesignFactory.java
+++ src/util/OilImpDesignFactory.java
@@ -2,6 +2,7 @@ package util;
 
 import java.awt.Font;
 import java.awt.Color;
+import java.text.DecimalFormat;
 import javax.swing.BorderFactory;
 import javax.swing.border.*;
 
@@ -16,12 +17,14 @@ public class OilImpDesignFactory
     public static final Font FONT_ARIAL_18 = new Font("Arial", Font.PLAIN, 18);
     public static final Font FONT_COMIC_18_BI = new Font("Comic Sans MS", Font.ITALIC | Font.BOLD, 18);
     public static final Font FONT_CNEW_12 = new Font("Courier New", Font.PLAIN, 12);
+    public static final Font FONT_OCR_18 = new Font("OCR A Extended", Font.PLAIN, 18);
 
     public static final Font BORDER_FONT = FONT_CALIBRI_22_B;
     public static final Font BORDER_SMALL_FONT = FONT_ROMAN_14;
     public static final Font LABEL_FONT = FONT_ARIAL_18;
     public static final Font LOG_FONT = FONT_CNEW_12;
     public static final Font HEADLINE_FONT = FONT_COMIC_18_BI;
+    public static final Font NUMBER_FONT = FONT_OCR_18;
 
     public static final Color backgroundColor = new Color(32, 188, 255);
 
@@ -40,4 +43,12 @@ public class OilImpDesignFactory
 
         return tb;
     }
+    
+    public static String formatBigNumber(double value)
+    {
+      String pattern = "###,###";
+      DecimalFormat myFormatter = new DecimalFormat(pattern);
+      String output = myFormatter.format(value);
+      return output;
+   }
 }
